b7692c53f74e490e171b4393ed7dbfe407140dd3
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2017-02-22 23:39:52 +0530

    working connection now

--- init.php
+++ init.php
@@ -2,7 +2,7 @@
 
 require_once "./vendor/autoload.php";
 
-$appdb = new \atk4\data\Persistence_SQL( \atk4\dsql\Connection::connect('mysql://root:winserver@localhost/atkui','root','winserver'));
+$appdb = new \atk4\data\Persistence_SQL( \atk4\dsql\Connection::connect('mysql:dbname=atkui;host=localhost','root','winserver'));
 
 $app= new Demo\Admin("Agile UI - Demo Application");
 $app->init();
