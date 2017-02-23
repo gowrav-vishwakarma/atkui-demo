3a0c99d69be4c3fe2f6c12ca21bdcf8935543504
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2017-02-23 13:49:48 +0530

    updated atk dependencies

--- lib/Model/SQL.php
+++ lib/Model/SQL.php
@@ -5,15 +5,17 @@ namespace Demo\Model;
 
 
 class SQL extends \atk4\data\Model {
+	use \atk4\core\AppScopeTrait;
+    use \atk4\core\FactoryTrait;
 	
 	public function __construct( $defaults = []){
-		global $appdb;	
-		$this->persistence = $appdb;	
-		parent::__construct($appdb, $defaults);
+		global $db;
+		$this->persistence = $db;	
+		parent::__construct($db, $defaults);
 	}
 
-	// public function init(){
-	// 	parent::init();
-	// 	$this->app->db  = $this->persistence;
-	// }
+	public function init(){
+		parent::init();
+		$this->app->db  = $this->persistence;
+	}
 }
